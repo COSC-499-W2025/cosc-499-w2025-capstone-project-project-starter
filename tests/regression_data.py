f3c8f1667c6e3692f258512555f9182c7ee138c6
luiswen <lwenluo@student.ubc.ca>
2025-11-09 21:06:58 -0800

    refactored test

--- tests/regression_data.py
+++
@@ -1,5 +0,0 @@
-def test_regression_output(tmp_path):
-    new_output = generate_report()
-    with open("tests/regression_data/reference_report.txt") as ref:
-        expected = ref.read()
-    assert new_output == expected
