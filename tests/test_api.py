379790d6b8074fce528375237efda4c813c04842
wjtilden <wtild952@mtroyal.ca>
2026-03-08 15:52:30 -0700

    tabs gone

--- tests/test_api.py
+++ tests/test_api.py
@@ -2098,4 +2098,3 @@ def test_portfolio_showcase_upsert_logic(engine):
         if isinstance(final_content, str):
             final_content = json.loads(final_content)
         assert final_content["description"] == "Updated Version"
-        
